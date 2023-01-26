782c8ef70861b1488a8ab5fc0b1974b500d2050f
Diego Farfan <diego_farfan20@hotmail.com>
2023-01-26 11:14:26 -0500

    Removed not needed out-of-the-box stuff

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { employeesRouter } from "./routers/employees";
 
 /**
@@ -8,7 +7,6 @@ import { employeesRouter } from "./routers/employees";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   empleados: employeesRouter,
 });
 
