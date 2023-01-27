08cd63a10ef214be1ce364535f7392c4b4a95829
Diego Farfan <diego_farfan20@hotmail.com>
2023-01-26 20:09:50 -0500

    Added basic error handling to queries

--- src/pages/empleados.tsx
+++ src/pages/empleados.tsx
@@ -5,22 +5,27 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Empleados: NextPage = () => {
-  const empleados = api.empleados.getAll.useQuery(undefined, {
+  const {
+    data: empleados,
+    isLoading,
+    isSuccess,
+  } = api.empleados.getAll.useQuery(undefined, {
     refetchOnMount: "always",
     staleTime: Infinity,
   });
-  const [filteredEmployees, setFilteredEmployees] = useState(empleados.data);
-
+  const [filteredEmployees, setFilteredEmployees] = useState(empleados);
   useEffect(() => {
-    setFilteredEmployees(empleados.data);
-  }, [empleados.data]);
+    setFilteredEmployees(empleados);
+  }, [empleados]);
 
   const filterEmployees = (query: string) => {
-    setFilteredEmployees(
-      empleados.data?.filter((employee) =>
-        employee.nombres.toLowerCase().includes(query.toLowerCase())
-      )
-    );
+    if (empleados) {
+      setFilteredEmployees(
+        empleados.filter((employee) =>
+          employee.nombres.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    }
   };
 
   return (
@@ -49,99 +54,105 @@ const Empleados: NextPage = () => {
           Agregar empleado
         </Link>
       </div>
-      <div className="relative overflow-x-auto border border-gray-50 shadow-md dark:border-gray-600 sm:rounded-lg">
-        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-          <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Nombre
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Cédula
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Correo
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Celular
-              </th>
-              <th scope="col" className="px-6 py-3">
-                EPS
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Estado
-              </th>
-            </tr>
-          </thead>
-          {filteredEmployees && (
-            <tbody>
-              {filteredEmployees.map((empleado) => (
-                <tr
-                  key={empleado.idEmpleado}
-                  className=" border-b bg-white last:border-b-0 hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800"
-                  // onClick={() => router.push(`/empleados/${empleado.cedula}`)}
-                >
-                  <th
-                    scope="row"
-                    className="whitespace-nowrap font-medium text-gray-900 dark:text-white"
+      {isLoading ? (
+        <p>Cargando...</p>
+      ) : isSuccess ? (
+        <div className="relative overflow-x-auto border border-gray-50 shadow-md dark:border-gray-600 sm:rounded-lg">
+          <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+            <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Nombre
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Cédula
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Correo
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Celular
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  EPS
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Estado
+                </th>
+              </tr>
+            </thead>
+            {filteredEmployees && (
+              <tbody>
+                {filteredEmployees.map((empleado) => (
+                  <tr
+                    key={empleado.idEmpleado}
+                    className=" border-b bg-white last:border-b-0 hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800"
+                    // onClick={() => router.push(`/empleados/${empleado.cedula}`)}
                   >
-                    <Link
-                      className="block px-6 py-4"
-                      href={`/empleados/${empleado.cedula}`}
+                    <th
+                      scope="row"
+                      className="whitespace-nowrap font-medium text-gray-900 dark:text-white"
                     >
-                      {empleado.nombres} {empleado.apellidos}
-                    </Link>
-                  </th>
-                  <td>
-                    <Link
-                      className="block px-6 py-4"
-                      href={`/empleados/${empleado.cedula}`}
+                      <Link
+                        className="block px-6 py-4"
+                        href={`/empleados/${empleado.cedula}`}
+                      >
+                        {empleado.nombres} {empleado.apellidos}
+                      </Link>
+                    </th>
+                    <td>
+                      <Link
+                        className="block px-6 py-4"
+                        href={`/empleados/${empleado.cedula}`}
+                      >
+                        {empleado.cedula}
+                      </Link>
+                    </td>
+                    <td>
+                      <Link
+                        className="block px-6 py-4"
+                        href={`/empleados/${empleado.cedula}`}
+                      >
+                        {empleado.email}
+                      </Link>
+                    </td>
+                    <td>
+                      <Link
+                        className="block px-6 py-4"
+                        href={`/empleados/${empleado.cedula}`}
+                      >
+                        {empleado.celular}
+                      </Link>
+                    </td>
+                    <td>
+                      <Link
+                        className="block px-6 py-4"
+                        href={`/empleados/${empleado.cedula}`}
+                      >
+                        {empleado.eps}
+                      </Link>
+                    </td>
+                    <td
+                      className={`${
+                        empleado.estado === "ACTIVO" ? "text-green-700" : ""
+                      }`}
                     >
-                      {empleado.cedula}
-                    </Link>
-                  </td>
-                  <td>
-                    <Link
-                      className="block px-6 py-4"
-                      href={`/empleados/${empleado.cedula}`}
-                    >
-                      {empleado.email}
-                    </Link>
-                  </td>
-                  <td>
-                    <Link
-                      className="block px-6 py-4"
-                      href={`/empleados/${empleado.cedula}`}
-                    >
-                      {empleado.celular}
-                    </Link>
-                  </td>
-                  <td>
-                    <Link
-                      className="block px-6 py-4"
-                      href={`/empleados/${empleado.cedula}`}
-                    >
-                      {empleado.eps}
-                    </Link>
-                  </td>
-                  <td
-                    className={`${
-                      empleado.estado === "ACTIVO" ? "text-green-700" : ""
-                    }`}
-                  >
-                    <Link
-                      className="block px-6 py-4"
-                      href={`/empleados/${empleado.cedula}`}
-                    >
-                      {empleado.estado}
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          )}
-        </table>
-      </div>
+                      <Link
+                        className="block px-6 py-4"
+                        href={`/empleados/${empleado.cedula}`}
+                      >
+                        {empleado.estado}
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            )}
+          </table>
+        </div>
+      ) : (
+        <p>Ha ocurrido un error</p>
+      )}
     </div>
   );
 };
